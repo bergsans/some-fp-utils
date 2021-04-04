6975ee04fb4dedc930b8a4d05b220f8f07923dd4
bergsans <bergclaesmagnus@gmail.com>
2021-04-04 21:23:41 +0200

    Inc compose args

--- lib/index.ts
+++ lib/index.ts
@@ -39,12 +39,16 @@ export const assocPath = <T>(path: string[]) => (
   return setValue(path, obj);
 };
 
-export const compose = <T1, T2, T3, T4, T5, T6>(
+export const compose = <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(
   fn1: (a: T1) => T2,
   fn2?: (a: T2) => T3,
   fn3?: (a: T3) => T4,
   fn4?: (a: T4) => T5,
-  fn5?: (a: T5) => T6
+  fn5?: (a: T5) => T6,
+  fn7?: (a: T6) => T7,
+  fn8?: (a: T7) => T8,
+  fn9?: (a: T9) => T10,
+  fn10?: (a: T10) => T11
 ) => {
   // @ts-ignore
   const fns = Array.from(arguments).filter((f) => f !== undefined);
